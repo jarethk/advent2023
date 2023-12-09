1b7da6a75d2ea243b134da66053e938bb631cc31
David Fuller <jarethk@shadowwebs.org>
2023-12-09 13:04:42 -0500

    Day 9 part 1 and 2 in js

---
+++ js/Day9/journal.md
@@ -0,0 +1,3 @@
+Part 1 - They really hand you the answer on this one.  For every set of numbers create a stack of reduced number sets with the difference, then walk that stack back adding the new end numbers.
+
+Part 2 - Same verse, except adding the new end numbers we subtract the new front numbers.  Too easy for Day 9.
---
+++ js/Day9/part1.mjs
@@ -0,0 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { open } from 'node:fs/promises';
+
+const debug = 0;
+const dl = log => { if (debug) console.log(log); };
+
+//const file = await open("./sample-input.txt");
+const file = await open("./full-input.txt");
+
+const t0 = performance.now();
+
+function reduceDiff(newNumArr, cv, idx, numArr) {
+    if (idx == 1) newNumArr = [];
+    newNumArr.push(cv - numArr[idx - 1]);
+    return newNumArr;
+}
+
+let nextSum = 0;
+
+for await (const line of file.readLines()) {
+    let nums = line.split(" ").map(v => Number.parseInt(v));
+    let numStacks = [];
+    while (nums.some(v => v != 0)) {
+        numStacks.push([...nums]);
+        nums = nums.reduce(reduceDiff);
+    }
+    dl(`Stacks: ${JSON.stringify(numStacks)}`);
+    let valAdd = 0;
+    while (numStacks.length > 0) {
+        nums = numStacks.pop();
+        valAdd = nums[nums.length - 1] + valAdd
+    }
+    nextSum += valAdd;
+}
+
+console.log(`Total added: ${nextSum}`);
+
+
+const t1 = performance.now();
+console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
